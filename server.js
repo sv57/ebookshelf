cd5c981bb6127c3b6133d79330ab934e1af9b7ab
sv57 <selva.vignesh98@gmail.com>
2020-10-30 15:43:38 +0530

    Initial AuthorRoutes

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
 app.set('view engine', 'ejs')
@@ -14,6 +17,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 
 
@@ -25,6 +29,8 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors', authorRouter)
+
 
 
 app.listen(process.env.PORT || 3000)
